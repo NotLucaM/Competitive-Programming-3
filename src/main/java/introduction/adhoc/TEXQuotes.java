d2183a1754dcc3c019bb9ad7aa59c9d12040847b
Luca <lm28073@pausd.us>
2020-06-01 17:48:07 -0700

    completed a problem from the introduction

--- src/main/java/introduction/adhoc/TEXQuotes.java
+++ src/main/java/introduction/adhoc/TEXQuotes.java
@@ -1,4 +1,26 @@
 package introduction.adhoc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TEXQuotes {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader((System.in)));
+
+        while (true) {
+           String line = reader.readLine();
+           if (line == null) {
+               break;
+           }
+            for (int i = 0; i < line.length(); i++) {
+                if (line.charAt(i) == '"') {
+                    System.out.print("''");
+                } else {
+                    System.out.print(line.charAt(i));
+                }
+            }
+        };
+    }
 }
